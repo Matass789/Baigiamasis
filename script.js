ceb6af276a666384ddd066196963eba01b1d6f4a
Matas <satam1998@gmail.com>
2023-05-04 11:11:44 +0300

    Karusele naujas

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+const myCarouselElement = document.querySelector('#hero-carousel')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 9000,
+  wrap: true,
+  pause: false,
+  
+})
+
+const myCarousel = document.getElementById('hero-carousel')
+
+myCarousel.addEventListener('slide.bs.carousel', event => {
+  direction("left")
+})
