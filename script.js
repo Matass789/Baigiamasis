7ad43be85e181b35d9daef0b30bb386e58a55dac
Matas <satam1998@gmail.com>
2023-05-08 13:42:48 +0300

    be stiliaus

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ const myCarousel = document.getElementById('hero-carousel')
 myCarousel.addEventListener('slide.bs.carousel', event => {
   direction("left")
 })
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
